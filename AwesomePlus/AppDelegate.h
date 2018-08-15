8a23ddd253345bef09b42ad243e05d109e16f9d6
xiaowei <wei.xiao02@hand-china.com>
2018-08-15 12:25:12 +0800

    init

---
+++ AwesomePlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AwesomePlus
+//
+//  Created by Mr.xiao on 2018/8/15.
+//  Copyright © 2018年 keepJion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AwesomePlus/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AwesomePlus
+//
+//  Created by Mr.xiao on 2018/8/15.
+//  Copyright © 2018年 keepJion. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
